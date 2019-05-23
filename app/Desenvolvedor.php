405c4a28302d8fa454d1f8dba61d36c3ad19e464
Thiago Maciel <thiagomrvieira@gmail.com>
2019-05-23 14:18:59 -0300

    Utilizando o belongsToMany

--- app/Desenvolvedor.php
+++ app/Desenvolvedor.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Desenvolvedor extends Model
 {
     protected $table = 'desenvolvedores';
+
+    public function projetos(){
+        return $this->belongsToMany("App\Projeto", "alocacoes");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Projeto;
+use App\Desenvolvedor;
+use App\Alocacao;
+
+Route::get('/desenvolvedor_projetos', function () {
+    $desenvolvedores = Desenvolvedor::with('projetos')->get();
+
+    foreach ($desenvolvedores as $d) {
+        echo "<p> Nome do desenvolvedor:". $d->nome ."</p>";
+        echo "<p> Cargo:". $d->cargo ."</p>";
+        if (count($d->projetos) > 0) {
+            echo "Projetos: <br>";
+            echo "<ul>";
+            foreach ($d->projetos as $p) {
+                echo "<li>";
+                echo "<b>Nome projeto</b>: " . $p->nome . " | ";
+                echo "<b>Estimativa horas</b>: " . $p->estimativa_horas ;
+                echo "</li>";
+            }
+            echo "</ul>";
+        }
+        echo "<hr>";
+    }
+
+    //return $desenvolvedores->toJson();
 });
